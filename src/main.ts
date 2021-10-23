3a1071109929bb25e0148925b94d3c70083f9da0
iBelando <ignaciobelando@gmail.com>
2021-10-23 15:17:52 -0300

    Implementation Utils

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/main/app.module';
 import { setSwagger } from './swagger';
 import * as Helmet from 'helmet';
 
