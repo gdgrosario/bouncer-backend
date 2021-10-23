69070ac450286a7cd1bbcef4a7dce73652bc1826
iBelando <ignaciobelando@gmail.com>
2021-10-23 15:07:14 -0300

    Initial Version

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { setSwagger } from './swagger';
+import * as Helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  setSwagger(app);
+  app.use(Helmet());
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = '/';
+export const SWAGGER_API_TITLE = "Bouncer's API";
+export const SWAGGER_API_DESCRIPTION = 'Developed by GDG Rosario';
+export const SWAGGER_API_VERSION = 'v1.0';
